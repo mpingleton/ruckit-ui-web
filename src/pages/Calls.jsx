048dc7a446f0aa0c2a05546f844d6b05beb6a5ba
Michael Pingleton <mnpingleton@gmail.com>
2021-12-25 13:35:12 -0600

    Added blank calls page.

---
+++ src/pages/Calls.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Stack } from '@mui/material';
+
+function Calls() {
+
+  return (
+    <Stack
+      direction="column"
+      alignItems="center"
+      sx={{ width: '100%', height: '100%' }}
+    >
+    </Stack>
+  );
+}
+
+export default Calls;
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -9,6 +9,7 @@ import BlankLayout from '../layouts/BlankLayout';
 import MainLayout from '../layouts/MainLayout';
 import Main from '../pages/Main';
 import Login from '../pages/Login';
+import Calls from '../pages/Calls';
 
 function AppRouter() {
   return (
@@ -16,7 +17,7 @@ function AppRouter() {
       <Routes>
         <Route path="/" element={<BlankLayout><Main /></BlankLayout>} />
         <Route path="/login" element={<BlankLayout><Login /></BlankLayout>} />
-        <Route path="/calls" element={<MainLayout></MainLayout>} />
+        <Route path="/calls" element={<MainLayout><Calls /></MainLayout>} />
       </Routes>
     </BrowserRouter>
   );
