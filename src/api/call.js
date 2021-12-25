ca28e78676b577539c17a36cd03b6bc97835b3ab
Michael Pingleton <mnpingleton@gmail.com>
2021-12-25 17:26:54 -0600

    Added API call to placeCall.

--- src/api/call.js
+++ src/api/call.js
@@ -1,4 +1,21 @@
 
+const placeCall = async (apiContext, callDetails) => {
+  const res = await fetch(apiContext.apiAddr + '/api/call', {
+    method: 'PUT',
+    headers: {
+      'Authorization': `Bearer ${apiContext.accessToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      pickupLocation: callDetails.pickupLocation,
+      dropoffLocation: callDetails.dropoffLocation,
+      mileage: callDetails.mileage,
+      riderId: callDetails.riderId,
+      driverId: callDetails.driverId,
+    }),
+  });
+};
+
 const getCalls = async (apiContext, status) => {
   const res = await fetch(apiContext.apiAddr + '/api/call?status=' + status, {
     method: 'GET',
@@ -11,5 +28,6 @@ const getCalls = async (apiContext, status) => {
 };
 
 module.exports = {
+  placeCall,
   getCalls,
 };
